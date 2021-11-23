4c2d305c0b6ac273f0244b6ba764117a8b7d84e0
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-11-23 21:24:33 +0100

    Add windows pop up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import './styles.css';
+/* eslint-disable import/no-cycle */
+import './css/styles.css';
 import Logo from './resources/meal-logo.png';
-
+import MealPhoto from './resources/food.jpg';
 const logoLink = document.getElementById('logo');
 logoLink.src = Logo;
+const MealImg = document.getElementById('recipe-img');
+MealImg.src = MealPhoto;
+const mealDetailsContent = document.querySelector('.meal-details');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+recipeCloseBtn.addEventListener('click', () => {
+  mealDetailsContent.style.display = 'none';
+});
