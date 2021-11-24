382e5cfecf08e2bee2211b9c4069423298f6895b
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-11-25 00:10:27 +0100

    Updated the Windows pop up

--- src/getMeals.js
+++ src/getMeals.js
@@ -1,15 +1,25 @@
-import Meals from './Meals.js';
+// import Meals from './Meals.js';
+// const getMeals = async () => {
+//   try {
+//     return await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese')
+//       .then((response) => response.json())
+//       .then((json) => {
+//         Meals.showBoard(json.meals);
+//       });
+//   } catch (e) {
+//     return null;
+//   }
+// };
+
+// export { getMeals as default };
 
 const getMeals = async () => {
-  try {
-    return await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese')
-      .then((response) => response.json())
-      .then((json) => {
-        Meals.showBoard(json.meals);
-      });
-  } catch (e) {
-    return null;
-  }
+  const response = await fetch(
+    'https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese',
+  );
+  const result = await response.json();
+  return result.meals;
 };
 
-export { getMeals as default };
+export default getMeals;
+
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@
 import './css/styles.css';
 import Logo from './resources/meal-logo.png';
 import getMeals from './getMeals.js';
+import mealBoard from './Meals.js'
 
 const logoLink = document.getElementById('logo');
 logoLink.src = Logo;
 
-document.addEventListener('DOMContentLoaded', getMeals());
+// document.addEventListener('DOMContentLoaded', getMeals());
+
+window.addEventListener('load', async () => {
+  mealBoard(await getMeals());
+});
