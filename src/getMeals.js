fa519d0bf14d4e700132cba8ff822559c3910bf9
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-11-25 17:16:02 +0100

    Undo the design to meet my coding partners intent design

--- src/getMeals.js
+++ src/getMeals.js
@@ -1,24 +1,15 @@
-// import Meals from './Meals.js';
-// const getMeals = async () => {
-//   try {
-//     return await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese')
-//       .then((response) => response.json())
-//       .then((json) => {
-//         Meals.showBoard(json.meals);
-//       });
-//   } catch (e) {
-//     return null;
-//   }
-// };
-
-// export { getMeals as default };
+import Meals from './Meals.js';
 
 const getMeals = async () => {
-  const response = await fetch(
-    'https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese',
-  );
-  const result = await response.json();
-  return result.meals;
+  try {
+    return await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese')
+      .then((response) => response.json())
+      .then((json) => {
+        Meals.showBoard(json.meals);
+      });
+  } catch (e) {
+    return null;
+  }
 };
 
-export default getMeals;
+export { getMeals as default };
