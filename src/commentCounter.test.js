b9d24c837f1a418592ba29f02664a64e65060673
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-11-25 20:40:01 +0100

    Add jest test for comment count

---
+++ src/commentCounter.test.js
@@ -0,0 +1,20 @@
+import commentCounter from './commentCounter.js';
+
+
+describe('test comment count', () => {
+  test('test number of comments from the Involvement API', () => {
+    const comment = [
+      {
+        comment: 'Good meal',
+        creation_date: '2021-11-25',
+        username: 'null',
+      },
+      {
+        comment: 'Great meal',
+        creation_date: '2021-11-25',
+        username: 'John Do',
+      },
+    ];
+    expect(commentCounter(comment)).toBe(2);
+  });
+});
